4f3d1c4cc20e7b8cbd9c70742448e865e225ac65
davidchau21 <davidduongxu1@gmail.com>
2024-01-04 18:54:49 +0700

    date time picker

---
+++ src/components/DateTimePickerComponent.tsx
@@ -0,0 +1,103 @@
+import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import TitleComponent from './TitleComponent';
+import RowComponent from './RowComponent';
+import TextComponent from './TextComponent';
+import {colors} from '../constants/colors';
+import {ArrowDown2} from 'iconsax-react-native';
+import {globalStyles} from '../styles/globalStyles';
+import {Modal} from 'react-native';
+import SpaceComponent from './SpaceComponent';
+import DatePicker from 'react-native-date-picker';
+
+interface Props {
+  type?: 'date' | 'time' | 'datetime';
+  title?: string;
+  placeholder?: string;
+  selected?: Date;
+  onSelect: (val: Date) => void;
+}
+
+const DateTimePickerComponent = (props: Props) => {
+  const {type, title, placeholder, selected, onSelect} = props;
+
+  const [isVisibleModalDateTime, setIsVisibleDateTime] = useState(false);
+
+  const [date, setDate] = useState(selected ?? new Date());
+
+  return (
+    <>
+      <View style={{marginBottom: 16}}>
+        {title && <TitleComponent text={title} />}
+        <RowComponent
+          onPress={() => setIsVisibleDateTime(true)}
+          styles={[
+            globalStyles.inputContainer,
+            {
+              marginTop: title ? 8 : 0,
+              paddingVertical: 16,
+            },
+          ]}>
+          <TextComponent
+            flex={1}
+            text={
+              selected
+                ? type === 'time'
+                  ? `${selected.getHours()}:${selected.getMinutes()}`
+                  : `${selected.getDate()}/${
+                      selected.getMonth() + 1
+                    }/${selected.getFullYear()}`
+                : placeholder
+                ? placeholder
+                : ''
+            }
+            color={selected ? colors.text : '#676767'}
+          />
+          <ArrowDown2 size={20} color={colors.text} />
+        </RowComponent>
+      </View>
+
+      <Modal visible={isVisibleModalDateTime} transparent animationType="slide">
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0,0,0,0.5)',
+          }}>
+          <View
+            style={{
+              margin: 20,
+              width: '80%',
+              backgroundColor: colors.white,
+              padding: 20,
+              borderRadius: 20,
+            }}>
+            <TitleComponent text="Date time picker" color={colors.blue} />
+            <View>
+              <DatePicker
+                date={date}
+                onDateChange={val => setDate(val)}
+                mode={type ?? 'datetime'}
+                locale="vi"
+              />
+            </View>
+            <SpaceComponent height={20} />
+            <Button
+              title="Confirm"
+              onPress={() => {
+                onSelect(date);
+                setIsVisibleDateTime(false);
+              }}
+            />
+            <Button title="Close" onPress={() => setIsVisibleDateTime(false)} />
+          </View>
+        </View>
+      </Modal>
+    </>
+  );
+};
+
+export default DateTimePickerComponent;
+
+const styles = StyleSheet.create({});
--- src/screens/tasks/AddNewTask.tsx
+++ src/screens/tasks/AddNewTask.tsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react'
-import Container from '../../components/Container'
-import TextComponent from '../../components/TextComponent'
-import { TaskModel } from '../../models/TaskModel'
-import SectionComponent from '../../components/SectionComponent'
-import InputComponent from '../../components/InputComponent'
-import { User } from 'iconsax-react-native'
-import { colors } from '../../constants/colors'
-import { Button } from 'react-native'
+import React, {useState} from 'react';
+import Container from '../../components/Container';
+import TextComponent from '../../components/TextComponent';
+import {TaskModel} from '../../models/TaskModel';
+import SectionComponent from '../../components/SectionComponent';
+import InputComponent from '../../components/InputComponent';
+import {User} from 'iconsax-react-native';
+import {colors} from '../../constants/colors';
+import {Button, View} from 'react-native';
+import DateTimePickerComponent from '../../components/DateTimePickerComponent';
+import RowComponent from '../../components/RowComponent';
+import SpaceComponent from '../../components/SpaceComponent';
 
 const initValue: TaskModel = {
   title: '',
   description: '',
-  dueDate: '',
-  start: '',
-  end: '',
+  dueDate: new Date(),
+  start: new Date(),
+  end: new Date(),
   uids: [],
   flieUrl: [],
-}
-
+};
 
 const AddNewTask = ({navigation}: any) => {
-  
   const [taskDetail, setTaskDetail] = useState<TaskModel>(initValue);
-  
-  const handleChangeValue = (id: string, value: string) => {
+
+  const handleChangeValue = (id: string, value: string | Date) => {
     const item: any = {...taskDetail};
 
     item[`${id}`] = value;
@@ -32,35 +33,61 @@ const AddNewTask = ({navigation}: any) => {
   };
 
   const handleAddNewTask = async () => {
-    console.log(taskDetail)
+    console.log(taskDetail);
   };
- 
+
   return (
     <Container back title="Add new task">
-        <SectionComponent>
-          <InputComponent 
-            value={taskDetail.title}
-            onChange={val => handleChangeValue('title', val)}
-            title='Title'
-            allowClear
-            placeholder='Title of task'
-          />
-          <InputComponent 
-            value={taskDetail.description}
-            onChange={val => handleChangeValue('description', val)}
-            title='Description'
-            allowClear
-            placeholder='Content'
-            multible
-            numberOfLines={3}
-          />
-        
-        </SectionComponent>
-        <SectionComponent>
-          <Button title='Save' onPress={handleAddNewTask}/>
-        </SectionComponent>
+      <SectionComponent>
+        <InputComponent
+          value={taskDetail.title}
+          onChange={val => handleChangeValue('title', val)}
+          title="Title"
+          allowClear
+          placeholder="Title of task"
+        />
+        <InputComponent
+          value={taskDetail.description}
+          onChange={val => handleChangeValue('description', val)}
+          title="Description"
+          allowClear
+          placeholder="Content"
+          multible
+          numberOfLines={3}
+        />
+        <DateTimePickerComponent
+          selected={taskDetail.dueDate}
+          onSelect={val => handleChangeValue('dueDate', val)}
+          placeholder="Choice"
+          type="date"
+          title="Due date"
+        />
+        <RowComponent>
+          <View style={{flex: 1}}>
+            <DateTimePickerComponent
+              type="time"
+              selected={taskDetail.start}
+              onSelect={val => handleChangeValue('', val)}
+              title="Start"
+            />
+          </View>
+          <SpaceComponent width={10} />
+          <View style={{flex: 1}}>
+            <DateTimePickerComponent
+              type="time"
+              selected={taskDetail.end}
+              onSelect={val => handleChangeValue('', val)}
+              title="End"
+              
+            />
+          </View>
+        </RowComponent>
+      </SectionComponent>
+      <SectionComponent>
+        <Button title="Save" onPress={handleAddNewTask} />
+      </SectionComponent>
     </Container>
-  )
-}
+  );
+};
 
-export default AddNewTask
+export default AddNewTask;
